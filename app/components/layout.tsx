895f670d00260bfc2e5912305958e882aad19918
JBANKS040 <stratos.ampz@gmail.com>
2025-04-17 18:13:23 +0200

    update layout/visuals

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -33,18 +33,17 @@ export default function Layout({ children }: LayoutProps) {
               Home
             </Link>
             <Link 
-              href="/projects"
+              href="/submissions"
               className={`flex items-center px-4 py-3 text-sm font-medium rounded-md transition-colors ${
-                router.pathname === '/projects'
+                router.pathname === '/submissions'
                   ? 'bg-gray-700 text-white'
                   : 'text-gray-300 hover:bg-gray-700 hover:text-white'
               }`}
             >
               <svg className="mr-3 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clipRule="evenodd" />
-                <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+                <path fillRule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h6v4H7V5zm6 6H7v2h6v-2z" clipRule="evenodd"/>
               </svg>
-              Projects
+              Submissions
             </Link>
           </nav>
         </div>
