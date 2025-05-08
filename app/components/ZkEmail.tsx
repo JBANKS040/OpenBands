8f065b5c859b7fae5780282ffc8faa46c3868e4d
masaun <masaun@protonmail.com>
2025-05-08 21:57:16 +0900

    [FE]: Adjust the regex in the extractEmailHeaderAndBody() - IN PROGRESS

--- app/components/ZkEmail.tsx
+++ app/components/ZkEmail.tsx
@@ -1,4 +1,5 @@
 //import { verifyDKIMSignature } from "@zk-email/helpers/dist/dkim"; /// [NOTE]: This modules would can work in only server-side. 
+import { ZkEmailSDKProvider } from "@zk-email/zk-email-sdk";
 
 import { headers } from "next/headers";
 
@@ -10,12 +11,32 @@ export async function extractEmailHeaderAndBody(
     //rawEmail: Buffer | string,
     params: InputGenerationArgs = {}
 ): Promise<{ header: string; body: string }> {
-    let header = "test header";
-    let body = "test body";
+    //let header = "test header";
+    //let body = "test body";
 
-    const match = rawEmail.match(/^Subject:\s*(.+)$/im);
-    console.log(`Subject: ${match}`);
+    /// @dev - @zk-email/zk-email-sdk
+    const input = await generateInputFromEmail(
+        "<slug-name>",
+        rawEmail,
+        {}
+    );
+    console.log(`input: ${input}`);
 
+    const header = rawEmail.match(/^Subject:\s*(.+)$/im);
+    //console.log(`header: ${header}`);
+
+    //const boundaryOfBeginning = "Content-Type: text/plain;";
+    //const boundaryOfEnd = "Content-Type:";
+    //const regex = new RegExp(`${boundaryOfBeginning}[\\r\\n]+([\\s\\S]*?)[\\r\\n]+${boundaryOfEnd}`, 'm');
+    //const body = rawEmail.match(regex);
+    const regex = /^Content-Type:\s*[text/plain]/im;
+    //const regex = /Content-Type:\s*text\/plain[^]*?(?:\r?\n){2}([^]*?)(?=\r?\n[-]+.*Content-Type:\s*text\/html)/i;
+    const body = rawEmail.match(regex);
+    //console.log(`body: ${body}`);
+
+
+    // const parts = rawEmail.split(/\r?\n\r?\n/); // split at first empty line
+    // console.log(`Email Body: ${parts}`);
 
     //console.log(`eml.split(""): ${rawEmail.split(" ")}`);
     //console.log(`eml.split("-----BEGIN PGP SIGNATURE-----")[1]: ${rawEmail.split("-----BEGIN PGP SIGNATURE-----")[1]}`);
@@ -41,6 +62,6 @@ export async function getDKIMResult(
 //   const dkimResult = await verifyDKIMSignature(rawEmail, undefined, undefined, true);
 //   console.log("DKIM Result:", dkimResult);
 //   //const { headers, body, bodyHash, publicKey, signature, modulusLength } = dkimResult;
-// 
+
 //   return dkimResult;
 }
