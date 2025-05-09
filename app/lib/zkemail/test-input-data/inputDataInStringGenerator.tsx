0170e0c90ce47eec707378bff2a2a4d00a3548b9
masaun <masaun@protonmail.com>
2025-05-09 14:30:01 +0900

    [FE]: Modify the arguments of HEADER and BODY in the inputDataInStringGenerator.tsx -> [Result]: Successful to generate a Proof - if a user input 'Senior Software Engineer' into the position field and input '100' into the salary field on UI

--- app/lib/zkemail/test-input-data/inputDataInStringGenerator.tsx
+++ app/lib/zkemail/test-input-data/inputDataInStringGenerator.tsx
@@ -4,8 +4,9 @@ import { pubkeyModulusFromJWK } from "@/lib/utils";
  * @notice - Generate an email header data in String format
  */
 export function getEmailHeaderInString() {
-    let HEADER = "from:runnier.leagues.0j@icloud.com\r\ncontent-type:text/plain; charset=utf-8\r\nmime-version:1.0 (Mac OS X Mail 16.0 \\(3731.500.231\\))\r\nsubject:Bitcoin\r\nmessage-id:<12800A90-4ECC-4513-9298-A33546122920@me.com>\r\ndate:Wed, 3 Apr 2024 16:23:48 +0530\r\nto:zkewtest@gmail.com\r\ndkim-signature:v=1; a=rsa-sha256; c=relaxed/relaxed; d=icloud.com; s=1a1hai; t=1712141644; bh=2JsdK4BMzzt9w4Zlz2TdyVCFc+l7vNyT5aAgGDYf7fM=; h=from:Content-Type:Mime-Version:Subject:Message-Id:Date:to; b=";
-    
+    //let HEADER = "from:runnier.leagues.0j@icloud.com\r\ncontent-type:text/plain; charset=utf-8\r\nmime-version:1.0 (Mac OS X Mail 16.0 \\(3731.500.231\\))\r\nsubject:Bitcoin\r\nmessage-id:<12800A90-4ECC-4513-9298-A33546122920@me.com>\r\ndate:Wed, 3 Apr 2024 16:23:48 +0530\r\nto:zkewtest@gmail.com\r\ndkim-signature:v=1; a=rsa-sha256; c=relaxed/relaxed; d=icloud.com; s=1a1hai; t=1712141644; bh=2JsdK4BMzzt9w4Zlz2TdyVCFc+l7vNyT5aAgGDYf7fM=; h=from:Content-Type:Mime-Version:Subject:Message-Id:Date:to; b=";
+    let HEADER = "Job Offer from AxBxC Corp";
+
     return HEADER;
 }
 
@@ -13,8 +14,9 @@ export function getEmailHeaderInString() {
  * @notice - Generate an email body data in String format
  */
 export function getEmailBodyInString() {
-    let BODY = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks\r\n\r\n15 years ago, Satoshi mined the first block of the Bitcoin blockchain =\r\nAfter the Bitcoin white paper appeared on October 31, 2008, on a =\r\ncryptography mailing list, the Genesis Block =E2=80=94 the first bitcoin =\r\nblock and the basis of the entire Bitcoin trading system in place to =\r\nthis day =E2=80=94 was mined on January 3, 2009.=20\r\n\r\nThe Genesis Block is also known as Block 0 or Block 1, and is still in =\r\nthe Bitcoin network, where it will remain as long as there is a computer =\r\nrunning the Bitcoin software.=20\r\n\r\nAll nodes in the Bitcoin network can consult it, even if it is at the =\r\nother end of the network with hundreds of thousands of blocks.\r\n";
-    
+    //let BODY = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks\r\n\r\n15 years ago, Satoshi mined the first block of the Bitcoin blockchain =\r\nAfter the Bitcoin white paper appeared on October 31, 2008, on a =\r\ncryptography mailing list, the Genesis Block =E2=80=94 the first bitcoin =\r\nblock and the basis of the entire Bitcoin trading system in place to =\r\nthis day =E2=80=94 was mined on January 3, 2009.=20\r\n\r\nThe Genesis Block is also known as Block 0 or Block 1, and is still in =\r\nthe Bitcoin network, where it will remain as long as there is a computer =\r\nrunning the Bitcoin software.=20\r\n\r\nAll nodes in the Bitcoin network can consult it, even if it is at the =\r\nother end of the network with hundreds of thousands of blocks.\r\n";
+    let BODY = "Dear Alice,\r\nWe are pleased to officially confirm your employment with AxBxC Corp. You will be joining us as a Senior Software Engineer, reporting to Bob David, starting on June 1.\r\nYour starting annual salary will be $100,000, payable on a annually basis, subject to standard deductions and withholdings. You will also be eligible for health insurance, as outlined in the employee handbook.\r\nWe’re excited to have you on board and look forward to seeing you grow with us. Please feel free to reach out if you have any questions before your start date.\r\nWelcome to the team!\r\nWarm regards,\r\nAlice Elisabeth\r\nSenior Software Engineer\r\nAxBxC Corp\r\n";
+
     return BODY;
 }
 
