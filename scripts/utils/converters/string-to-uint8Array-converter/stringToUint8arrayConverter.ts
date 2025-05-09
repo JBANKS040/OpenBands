fdf6d4880951b21f5afd860795b6be55f19bf542
masaun <masaun@protonmail.com>
2025-05-09 12:51:02 +0900

    [Script]: Add the emailHeaderInString, which is exported as the output.json

--- scripts/utils/converters/string-to-uint8Array-converter/stringToUint8arrayConverter.ts
+++ scripts/utils/converters/string-to-uint8Array-converter/stringToUint8arrayConverter.ts
@@ -1,27 +1,32 @@
 import * as fs from "fs";
 
-import { getEmailBodyInString } from "./input/inputDataInStringGenerator.ts";
+import { getEmailHeaderInUint8Array, getEmailBodyInString } from "./input/inputDataInStringGenerator.ts";
 
 /**
  * @notice - The main function
  */
 async function main(): Promise<String> { // Mark the function as async
+    const HEADER = await getEmailHeaderInString();
     const BODY = await getEmailBodyInString();
-    
+
+    const emailHeaderInString = new String(HEADER);
     const emailBodyInString = new String(BODY);
 
     /// @dev - Convert String to Uint8Array
     const encoder = new TextEncoder();
+    const emailHeaderInUint8array = encoder.encode(emailHeaderInString);
     const emailBodyInUint8array = encoder.encode(emailBodyInString);
-    console.log(`Email Body (in Uint8Array):\n ${ emailBodyInUint8array }`); // Output: "Hello, World!"
+    console.log(`Email Header (in Uint8Array):\n ${ emailHeaderInUint8array }`); // Output: "Hello, World!"
+    console.log(`Email Body (in Uint8Array):\n ${ emailBodyInUint8array }`);     // Output: "Hello, World!"
 
     /// @dev - Export as a JSON file
     const result = {
+        emailHeaderInUint8array: emailHeaderInUint8array,
         emailBodyInUint8array: emailBodyInUint8array,
     };
     exportJSON(result, "scripts/utils/converters/string-to-uint8Array-converter/output/output.json");
 
-    return emailBodyInUint8array; // Return the resolved value
+    return { emailHeaderInUint8array, emailBodyInUint8array }; // Return the resolved value
 }
 
 /**
