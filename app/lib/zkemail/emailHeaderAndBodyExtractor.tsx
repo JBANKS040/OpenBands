5530e4cfcd0d678b15aa29fc5916bb407915e3d3
masaun <masaun@protonmail.com>
2025-05-11 10:06:50 +0900

    [FE]: Rename the 'headerAndBodyWithoutHtmlPart' with the 'rawEmailWithoutHtmlPart'

--- app/lib/zkemail/emailHeaderAndBodyExtractor.tsx
+++ app/lib/zkemail/emailHeaderAndBodyExtractor.tsx
@@ -17,15 +17,15 @@ export async function extractEmailHeaderAndBody(
     // const body = rawEmail.match(regexForBodyExtraction);
 
     /// @dev - Extract the email header and body, which the HTML part is cut off, from the entire (raw) email text.
-    const headerAndBodyWithoutHtmlPart = extractHeaderAndBodyWithoutHtmlPart(rawEmail);
-    console.log(`headerAndBodyWithoutHtmlPart: ${headerAndBodyWithoutHtmlPart}`);
+    const rawEmailWithoutHtmlPart = extractRawEmailWithoutHtmlPart(rawEmail);
+    console.log(`rawEmailWithoutHtmlPart: ${rawEmailWithoutHtmlPart}`);
 
     /// @dev - Extract the email body, which the below part of the "Content-Type: text/plain" is cut off from the "headerAndBodyWithoutHtmlPart".
-    const bodyWithoutHeader = extractBodyWithoutHeader(headerAndBodyWithoutHtmlPart);
+    const bodyWithoutHeader = extractBodyWithoutHeader(rawEmailWithoutHtmlPart);
     console.log(`bodyWithoutHeader: ${bodyWithoutHeader}`);
 
     /// @dev - Extract the email header, which the above part of the "Content-Type: text/plain" is cut off from the "headerAndBodyWithoutHtmlPart".  
-    const headerWithoutBody = extractHeaderWithoutBody(headerAndBodyWithoutHtmlPart);
+    const headerWithoutBody = extractHeaderWithoutBody(rawEmailWithoutHtmlPart);
     console.log(`headerWithoutBody: ${headerWithoutBody}`);
 
     const header = headerWithoutBody;
@@ -37,25 +37,25 @@ export async function extractEmailHeaderAndBody(
 /** 
  * @dev - Extract the email header and body, which the HTML part is cut off, from the entire (raw) email text.
  */
-function extractHeaderAndBodyWithoutHtmlPart(rawEmail: string) {
+function extractRawEmailWithoutHtmlPart(rawEmail: string) {
     const delimiter = "Content-Type: text/html;";
     const index = rawEmail.indexOf(delimiter);
     if (index === -1) return rawEmail; // Fallback if not found
     // Return everything before the delimiter
-    const headerAndBodyWithoutHtmlPart = rawEmail.substring(0, index).trim();
-    console.log(`headerAndBodyWithoutHtmlPart: ${headerAndBodyWithoutHtmlPart}`);
-    return headerAndBodyWithoutHtmlPart;
+    const rawEmailWithoutHtmlPart = rawEmail.substring(0, index).trim();
+    console.log(`rawEmailWithoutHtmlPart: ${rawEmailWithoutHtmlPart}`);
+    return rawEmailWithoutHtmlPart;
 }
 
 /** 
  * @dev - Extract the email body, which the below part of the "Content-Type: text/plain" is cut off from the "headerAndBodyWithoutHtmlPart".
  */
-function extractBodyWithoutHeader(headerAndBodyWithoutHtmlPart: string) {
+function extractBodyWithoutHeader(rawEmailWithoutHtmlPart: string) {
     const delimiter = 'Content-Type: text/plain;';
-    const indexOfContentTypeTextPlain = headerAndBodyWithoutHtmlPart.indexOf(delimiter);
+    const indexOfContentTypeTextPlain = rawEmailWithoutHtmlPart.indexOf(delimiter);
     if (indexOfContentTypeTextPlain === -1) return ''; // fallback: return full text if delimiter not found
     // Move past the delimiter and any trailing newlines or spaces
-    const _bodyWithoutHeader = headerAndBodyWithoutHtmlPart.slice(indexOfContentTypeTextPlain + delimiter.length);
+    const _bodyWithoutHeader = rawEmailWithoutHtmlPart.slice(indexOfContentTypeTextPlain + delimiter.length);
     const bodyWithoutHeader = _bodyWithoutHeader.trimStart();
     console.log(`bodyWithoutHeader: ${bodyWithoutHeader}`);
     return bodyWithoutHeader;
@@ -64,12 +64,12 @@ function extractBodyWithoutHeader(headerAndBodyWithoutHtmlPart: string) {
 /** 
  * @dev - Extract the email header, which the above part of the "Content-Type: text/plain" is cut off from the "headerAndBodyWithoutHtmlPart".
  */
-function extractHeaderWithoutBody(headerAndBodyWithoutHtmlPart: string) {
+function extractHeaderWithoutBody(rawEmailWithoutHtmlPart: string) {
     const delimiter = 'Content-Type: text/plain;';
-    const indexOfContentTypeTextPlain = headerAndBodyWithoutHtmlPart.indexOf(delimiter);
+    const indexOfContentTypeTextPlain = rawEmailWithoutHtmlPart.indexOf(delimiter);
     if (indexOfContentTypeTextPlain === -1) return ''; // fallback: return full text if delimiter not found
     // Move past the delimiter and any trailing newlines or spaces
-    const headerWithoutBody = headerAndBodyWithoutHtmlPart.substring(0, indexOfContentTypeTextPlain).trim(); // Extract everything before the delimiter
+    const headerWithoutBody = rawEmailWithoutHtmlPart.substring(0, indexOfContentTypeTextPlain).trim(); // Extract everything before the delimiter
     console.log(`headerWithoutBody: ${headerWithoutBody}`);
     return headerWithoutBody;
 }
