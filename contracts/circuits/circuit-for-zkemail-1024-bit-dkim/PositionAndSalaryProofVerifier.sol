a20a9b47ac76d15aa0d6004fcb7e76a18535102f
masaun <masaun@protonmail.com>
2025-06-09 09:27:02 +0900

    [SC (1024 & 2048)]: Define the PositionAndSalaryProofVerifier.sol

---
+++ contracts/circuits/circuit-for-zkemail-1024-bit-dkim/PositionAndSalaryProofVerifier.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.8.21;
+
+import { HonkVerifier } from "./honk-verifier/plonk_vk.sol";
+
+contract PositionAndSalaryProofVerifier {
+    HonkVerifier public verifier;
+
+    constructor(HonkVerifier _verifier) {
+        verifier = _verifier;
+    }
+
+    function verifyPositionAndSalaryProof(bytes calldata proof, bytes32[] calldata publicInputs) public view returns (bool) {
+        bool proofResult = verifier.verify(proof, publicInputs);
+        require(proofResult, "Proof is not valid");
+        return proofResult;
+    }
+}
