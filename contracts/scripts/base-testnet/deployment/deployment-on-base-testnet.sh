494ebb0b494553ab669be841ad3808634e7f3bea
masaun <masaun@protonmail.com>
2025-06-14 17:59:57 +0900

    [SC deployment]: Comment out the SC verification command for BaseScan

--- contracts/scripts/base-testnet/deployment/deployment-on-base-testnet.sh
+++ contracts/scripts/base-testnet/deployment/deployment-on-base-testnet.sh
@@ -34,35 +34,35 @@ forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
 # @notice - The following commands are used to verify the deployed contracts on Base Sepolia Testnet Explorer. #
 ################################################################################################################
 
-echo "Verify the deployed-HonkVerifier and PositionAndSalaryProof1024Verifier contract on Base Sepolia Testnet Explorer..."
-forge script/base-testnet/deployment/DeploymentForHonkVerifier1024.s.sol \
-    --rpc-url ${BASE_TESTNET_RPC} \
-    --chain-id ${BASE_TESTNET_CHAIN_ID} \
-    --private-key ${BASE_TESTNET_PRIVATE_KEY} \
-    --resume \
-    --verify \
-    --verifier etherscan \
-    --verifier-url https://api-sepolia.basescan.org/api \
-    --etherscan-api-key ${BASESCAN_API_KEY} \
+# echo "Verify the deployed-HonkVerifier and PositionAndSalaryProof1024Verifier contract on Base Sepolia Testnet Explorer..."
+# forge script/base-testnet/deployment/DeploymentForHonkVerifier1024.s.sol \
+#     --rpc-url ${BASE_TESTNET_RPC} \
+#     --chain-id ${BASE_TESTNET_CHAIN_ID} \
+#     --private-key ${BASE_TESTNET_PRIVATE_KEY} \
+#     --resume \
+#     --verify \
+#     --verifier etherscan \
+#     --verifier-url https://api-sepolia.basescan.org/api \
+#     --etherscan-api-key ${BASESCAN_API_KEY} \
 
-echo "Verify the deployed-HonkVerifier and PositionAndSalaryProof2048Verifier contract on Base Sepolia Testnet Explorer..."
-forge script script/base-testnet/deployment/DeploymentForHonkVerifier2048.s.sol \
-    --rpc-url ${BASE_TESTNET_RPC} \
-    --chain-id ${BASE_TESTNET_CHAIN_ID} \
-    --private-key ${BASE_TESTNET_PRIVATE_KEY} \
-    --resume \
-    --verify \
-    --verifier etherscan \
-    --verifier-url https://api-sepolia.basescan.org/api \
-    --etherscan-api-key ${BASESCAN_API_KEY} \
+# echo "Verify the deployed-HonkVerifier and PositionAndSalaryProof2048Verifier contract on Base Sepolia Testnet Explorer..."
+# forge script script/base-testnet/deployment/DeploymentForHonkVerifier2048.s.sol \
+#     --rpc-url ${BASE_TESTNET_RPC} \
+#     --chain-id ${BASE_TESTNET_CHAIN_ID} \
+#     --private-key ${BASE_TESTNET_PRIVATE_KEY} \
+#     --resume \
+#     --verify \
+#     --verifier etherscan \
+#     --verifier-url https://api-sepolia.basescan.org/api \
+#     --etherscan-api-key ${BASESCAN_API_KEY} \
 
-echo "Verify the deployed-PositionAndSalaryProofManager contract on Base Sepolia Testnet Explorer..."
-forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
-    --rpc-url ${BASE_TESTNET_RPC} \
-    --chain-id ${BASE_TESTNET_CHAIN_ID} \
-    --private-key ${BASE_TESTNET_PRIVATE_KEY} \
-    --resume \
-    --verify \
-    --verifier etherscan \
-    --verifier-url https://api-sepolia.basescan.org/api \
-    --etherscan-api-key ${BASESCAN_API_KEY} \
+# echo "Verify the deployed-PositionAndSalaryProofManager contract on Base Sepolia Testnet Explorer..."
+# forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
+#     --rpc-url ${BASE_TESTNET_RPC} \
+#     --chain-id ${BASE_TESTNET_CHAIN_ID} \
+#     --private-key ${BASE_TESTNET_PRIVATE_KEY} \
+#     --resume \
+#     --verify \
+#     --verifier etherscan \
+#     --verifier-url https://api-sepolia.basescan.org/api \
+#     --etherscan-api-key ${BASESCAN_API_KEY} \
