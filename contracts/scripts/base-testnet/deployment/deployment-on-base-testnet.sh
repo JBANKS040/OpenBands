cf6a7b5092361b1b1c3eaac2d0a82219fe5b3042
masaun <masaun@protonmail.com>
2025-06-14 12:05:45 +0900

    [SC]: Add the SC verification command on BASE Sepolia Testnet Explorer (BaseScan)

--- contracts/scripts/base-testnet/deployment/deployment-on-base-testnet.sh
+++ contracts/scripts/base-testnet/deployment/deployment-on-base-testnet.sh
@@ -30,7 +30,33 @@ forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
 
 # [NOTE - Adding the "--legacy" option]: Due to this error - Error: Failed to estimate EIP1559 fees. This chain might not support EIP1559, try adding --legacy to your command.
 
-echo "Verify the deployed-smart contracts on Base Sepolia Testnet Explorer..."
+################################################################################################################
+# @notice - The following commands are used to verify the deployed contracts on Base Sepolia Testnet Explorer. #
+################################################################################################################
+
+echo "Verify the deployed-HonkVerifier and PositionAndSalaryProof1024Verifier contract on Base Sepolia Testnet Explorer..."
+forge script/base-testnet/deployment/DeploymentForHonkVerifier1024.s.sol \
+    --rpc-url ${BASE_TESTNET_RPC} \
+    --chain-id ${BASE_TESTNET_CHAIN_ID} \
+    --private-key ${BASE_TESTNET_PRIVATE_KEY} \
+    --resume \
+    --verify \
+    --verifier etherscan \
+    --verifier-url https://api-sepolia.basescan.org/api \
+    --etherscan-api-key ${BASESCAN_API_KEY} \
+
+echo "Verify the deployed-HonkVerifier and PositionAndSalaryProof2048Verifier contract on Base Sepolia Testnet Explorer..."
+forge script script/base-testnet/deployment/DeploymentForHonkVerifier2048.s.sol \
+    --rpc-url ${BASE_TESTNET_RPC} \
+    --chain-id ${BASE_TESTNET_CHAIN_ID} \
+    --private-key ${BASE_TESTNET_PRIVATE_KEY} \
+    --resume \
+    --verify \
+    --verifier etherscan \
+    --verifier-url https://api-sepolia.basescan.org/api \
+    --etherscan-api-key ${BASESCAN_API_KEY} \
+
+echo "Verify the deployed-PositionAndSalaryProofManager contract on Base Sepolia Testnet Explorer..."
 forge script script/base-testnet/deployment/DeploymentAllContracts.s.sol \
     --rpc-url ${BASE_TESTNET_RPC} \
     --chain-id ${BASE_TESTNET_CHAIN_ID} \
