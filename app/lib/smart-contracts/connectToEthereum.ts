07a3ea095edc9ad3890d94d49fe29c92f8dda93a
masaun <masaun@protonmail.com>
2025-06-18 16:48:54 +0900

    [FE]: Fix indent of the Window interface

--- app/lib/smart-contracts/connectToEthereum.ts
+++ app/lib/smart-contracts/connectToEthereum.ts
@@ -1,10 +1,5 @@
-// Import everything
 import { ethers } from "ethers";
-
-// Import just a few select items
 import { BrowserProvider, parseUnits } from "ethers";
-
-// Import from a specific export
 import { HDNodeWallet } from "ethers/wallet";
 
 // @dev - The global declaration for the EthereumProvider interface.
@@ -15,10 +10,9 @@ declare global {
     }
 
     interface Window {
-    ethereum?: EthereumProvider;
-  }
+        ethereum?: EthereumProvider;
+    }
 }
-
 
 /**
  * @notice - Connect to Ethereum
@@ -26,7 +20,7 @@ declare global {
  */
 export async function connectToEthereum(): Promise<{ provider: any, signer: any }> {
     let signer: any = null;
-    let provider: any;
+    let provider: any = null;
 
     if (window.ethereum == null) {
         // If MetaMask is not installed, we use the default provider, which is backed by a variety of third-party services (such as INFURA). 
@@ -44,3 +38,26 @@ export async function connectToEthereum(): Promise<{ provider: any, signer: any 
 
     return { provider, signer }; // Return the resolved value
 }
+
+
+
+
+//////////////////////////////////////////////////////////////////// 
+// Example usage of ethers.js utilities (v6) for unit conversions //
+////////////////////////////////////////////////////////////////////
+
+// // Convert user-provided strings in ether to wei for a value
+// eth = parseEther("1.0")
+// // 1000000000000000000n
+
+// // Convert user-provided strings in gwei to wei for max base fee
+// feePerGas = parseUnits("4.5", "gwei")
+// // 4500000000n
+
+// // Convert a value in wei to a string in ether to display in a UI
+// formatEther(eth)
+// // '1.0'
+
+// // Convert a value in wei to a string in gwei to display in a UI
+// formatUnits(feePerGas, "gwei")
+// // '4.5'
