92457ba59ebb72c1e24746a42e3dc42184e4f84a
masaun <masaun@protonmail.com>
2025-06-18 18:00:57 +0900

    [FE]: Store the provider into the window.ethereum in the connectToEthereum()

--- app/lib/smart-contracts/evm/connectToEthereum.ts
+++ app/lib/smart-contracts/evm/connectToEthereum.ts
@@ -20,17 +20,18 @@ import { EthereumProvider, Window } from "./dataTypes";
  * @notice - Connect to Ethereum
  * @dev - ref). https://docs.ethers.io/v6/getting-started/#getting-started-connecting
  */
-export async function connectToEthereum(provider: EthereumProvider): Promise<{ provider: any, signer: any }> {
+export async function connectToEthereum(): Promise<{ provider: any, signer: any }> {
   let signer: any = null;
-  //let provider: any = null;
+  let provider: any = null;
 
-  let window: Window = { ethereum: provider || null };
+  let window: Window = { ethereum: null };
 
   if (window.ethereum == null) {
     // If MetaMask is not installed, we use the default provider, which is backed by a variety of third-party services (such as INFURA). 
     // They do not have private keys installed, so they only have read-only access
     console.log("MetaMask not installed; using read-only defaults")
     provider = ethers.getDefaultProvider()
+    window.ethereum = provider;
   } else {
     // Connect to the MetaMask EIP-1193 object. 
     // This is a standard protocol that allows Ethers access to make all read-only requests through MetaMask.
