f09301bd8741392a099e25dc323528c32b7625fe
masaun <masaun@protonmail.com>
2025-06-20 15:51:04 +0900

    [SC]: Add the Copy & Paste command of the artifacts (icl. ABIs) to the compileContracts.sh

--- contracts/compileContracts.sh
+++ contracts/compileContracts.sh
@@ -1,2 +1,5 @@
 echo "Compiling the smart contracts..."
 forge build --evm-version cancun
+
+echo "Copy the artifacts (icl. ABIs) into the ./app/lib/smart-contracts/evm/smart-contracts/artifacts directory"
+cp -r out/* ../app/lib/smart-contracts/evm/smart-contracts/artifacts
