17a1f93bd966ae227fbd9f1ac3215aec0342827e
masaun <masaun@protonmail.com>
2025-07-03 18:05:12 +0900

    [FE]: Add 'export' keyword to the uint8ArrayToHex()

--- app/lib/converters/uint8ArrayProofToHexStringProofConverter.ts
+++ app/lib/converters/uint8ArrayProofToHexStringProofConverter.ts
@@ -1,7 +1,7 @@
 /**
  * @notice - To convert a Uint8Array proof to a hex string (proofHex) suitable for passing to a Solidity verifier, you can use the following function in TypeScript/JavaScript:
  */
-function uint8ArrayToHex(proof: Uint8Array): string {
+export function uint8ArrayToHex(proof: Uint8Array): string {
   return '0x' + Array.from(proof)
     .map(b => b.toString(16).padStart(2, '0'))
     .join('');
